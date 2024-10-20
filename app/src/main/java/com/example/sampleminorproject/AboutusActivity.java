93a9c546ea2c1d1f4ed77777da4bae0db13b732b
PreetishMajumdar <preetishmajumdar@gmail.com>
2024-10-20 22:46:57 +0530

    about us page done

--- app/src/main/java/com/example/sampleminorproject/AboutusActivity.java
+++ app/src/main/java/com/example/sampleminorproject/AboutusActivity.java
@@ -2,13 +2,29 @@ package com.example.sampleminorproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class AboutusActivity extends AppCompatActivity {
+
+    ImageView backarrow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aboutus);
+
+
+        backarrow=findViewById(R.id.backarrow);
+        backarrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AboutusActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
