b81e2399a0596998eaf1e55510494e84f93a0b49
PreetishMajumdar <preetishmajumdar@gmail.com>
2024-10-20 23:08:40 +0530

    leftright animations

--- app/src/main/java/com/example/sampleminorproject/AboutusActivity.java
+++ app/src/main/java/com/example/sampleminorproject/AboutusActivity.java
@@ -5,11 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 public class AboutusActivity extends AppCompatActivity {
 
-    ImageView backarrow;
+    ImageView backarrow, toplogo;
+    TextView topbackground;
+    ScrollView aboutpreetishscroll, abouthemanthscroll;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +24,11 @@ public class AboutusActivity extends AppCompatActivity {
 
 
         backarrow=findViewById(R.id.backarrow);
+        toplogo=findViewById(R.id.toplogo);
+        topbackground=findViewById(R.id.topbackground);
+        aboutpreetishscroll=findViewById(R.id.aboutpreetishscroll);
+        abouthemanthscroll=findViewById(R.id.abouthemanthscroll);
+
         backarrow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -25,6 +36,22 @@ public class AboutusActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
+        Animation topToDown = AnimationUtils.loadAnimation(this, R.anim.toptodown);
+        Animation fadeIn = AnimationUtils.loadAnimation(this, R.anim.fadein);
+        Animation downToTop = AnimationUtils.loadAnimation(this, R.anim.downtotop);
+        Animation lefttoright = AnimationUtils.loadAnimation(this, R.anim.lefttoright);
+        Animation righttoleft = AnimationUtils.loadAnimation(this, R.anim.righttoleft);
+
+
+
+        toplogo.startAnimation(topToDown);
+        topbackground.startAnimation(topToDown);
+        backarrow.startAnimation(topToDown);
+        aboutpreetishscroll.startAnimation(lefttoright);
+        abouthemanthscroll.startAnimation(righttoleft);
+
 
     }
 }
